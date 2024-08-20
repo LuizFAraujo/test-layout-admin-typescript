a6eb3242daaa8da5cf3f9f1da5ee4f8236a6b0e3
LuizFAraujo <luizfaraujo.eng@gmail.com>
2024-08-20 01:14:42 -0300

    Configuração básica

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-600 via-gray-300 to-blue-500">
+      <div className="text-center p-10 bg-white bg-opacity-90 rounded-lg shadow-lg">
+        <h1 className="text-6xl font-extrabold text-gray-800 tracking-wide">
+          ArjSYS
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
